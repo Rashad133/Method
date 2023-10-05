062254b804f32c31058261d97e02411c9b114a0e
Rashad133 <tu82907hw@code.edu.az>
2023-10-05 05:53:22 +0400

    Adding file

---
+++ MethodsTasks/MethodsTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodsTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
