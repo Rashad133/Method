4f7440781ba4c23954c0cc7bbbce804bd606d039
Rashad133 <tu82907hw@code.edu.az>
2023-10-05 05:56:38 +0400

    uptade

--- MethodsTasks/MethodsTasks/Program.cs
+++ MethodsTasks/MethodsTasks/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace MethodsTasks
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//TASK 1//
+
+//int[] numbers = { 4, 6, 32, 52, 68 };
+//int minNumber = Min(numbers);
+
+//Console.WriteLine(minNumber);
+
+// static int Min(int[] arr)
+//{
+//   int min = arr[0];
+
+//   for (int i = 0; i < arr.Length; i++)
+//   {
+//     if (arr[i]<min)
+//     {
+//         min = arr[i];
+//     }
+//   }
+//    return min;
+//}
+
+
+//TASK 2//
+//public static int Area(int radius)
+//{
+//    int p = 3;
+//     int r = radius * radius;
+//    int s = p * r;
+//    return s;
+//}
+
+//public static int Area(int a, int b)
+//{
+//    int s = a * b;
+//    return s;
+//}
+
+//public static int Area(int a, int b, int c)
+//{
+//    int s = 2 * ((a * b) + (a * c) + (b * c));
+//    return s;
+//}
+
+//public static int Area(int a, int b, int c)
+//{
+//    int s = (a + b + c) / 2 * radius;
+//    return s;
+//}
+
